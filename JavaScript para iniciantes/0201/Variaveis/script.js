55dec51bd84b8db64225b6f9fb52fb30f1d7b085
AndreLucasv10 <andrelucasv10@gmail.com>
2023-08-30 20:51:28 -0300

    Pequeno estudo de variaveis

---
+++ JavaScript para iniciantes/0201/Variaveis/script.js
@@ -0,0 +1,36 @@
+/* Exemplo 1 */
+
+let nome = "André";
+let idade = 28;
+let possuiFaculdade = true;
+console.log(nome, idade, possuiFaculdade);
+
+/*Exemplo 2 */
+
+let preco = 25;
+let totalComprado = 5;
+let totalPreco = totalComprado * preco;
+
+console.log(totalPreco);
+
+/* Exemplo 3 */
+
+let sobrenome = "Rafael",
+  cidade = "Rio";
+
+console.log(sobrenome, cidade);
+
+/* Exemplo 4 */
+
+let semDefinir;
+console.log(semDefinir);
+
+// Exemplo 5
+
+console.log(comida);
+var comida = "pizza";
+console.log(comida);
+// Exemplo 6
+
+let time = "Vasco";
+console.log(time);
---
+++ JavaScript para iniciantes/Exercicio/script.js
@@ -0,0 +1,13 @@
+// Declarar uma variável com o seu nome
+let nome = "André";
+// Declarar uma variável com a sua idade
+let idade = 21;
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+let comidaFavorita;
+// Atribuir valor a sua comida favorita
+comidaFavorita = "Hanburguer";
+// Declarar 5 variáveis diferentes sem valores
+let A, B, C, D, E;
+
+console.log(nome, idade, comidaFavorita);
