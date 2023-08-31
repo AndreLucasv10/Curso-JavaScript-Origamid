f0958a791b7baf6346f45f6bd37cbe7c8bc2f200
AndreLucasv10 <andrelucasv10@gmail.com>
2023-08-31 14:00:29 -0300

    Começo dos estudos

---
+++ JavaScript para iniciantes/0201/Número e Operadores 1/script.js
@@ -0,0 +1,14 @@
+//Exemplo 1
+
+var idade = 2e10;
+console.log(idade);
+
+//Exemplo 2
+
+var total = 10 + 5 + 10;
+var divisao = 200 / 5;
+var modulo = 34687468 / 3;
+
+console.log(modulo);
+
+//Exemplo 3
---
+++ JavaScript para iniciantes/0201/Tipos de dados/Exercicio/script.js
@@ -0,0 +1,17 @@
+// Declare uma variável contendo uma string
+var nome = "André";
+console.log(typeof nome);
+// Declare uma variável contendo um número dentro de uma string
+let nomeIdade = "André " + 28 + " anos";
+// Declare uma variável com a sua idade
+let idade = 22;
+console.log(typeof idade);
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var nome = "André";
+var sobrenome = "Lucas";
+var nomeSobrenome = ` ${nome} ${sobrenome}`;
+// Coloque a seguinte frase em uma variável: It's time
+var time = "It's time";
+// Verifique o tipo da variável que contém o seu nome
+console.log(nome);
