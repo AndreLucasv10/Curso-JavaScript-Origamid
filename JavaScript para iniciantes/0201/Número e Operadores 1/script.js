0a4339490ef51d0c98147d2a8f095458e095f3b2
AndreLucasv10 <andrelucasv10@gmail.com>
2023-08-31 17:39:32 -0300

    Estudo

--- JavaScript para iniciantes/0201/Número e Operadores 1/script.js
+++ JavaScript para iniciantes/0201/Número e Operadores 1/script.js
@@ -12,3 +12,23 @@ var modulo = 34687468 / 3;
 console.log(modulo);
 
 //Exemplo 3
+
+var total1 = 20 + 5 * 2; // 30
+var total2 = (20 + 5) * 2; // 50
+var total3 = (20 / 2) * 5; // 50
+var total4 = 10 + 10 * 2 + 20 / 2; // 40
+
+//Exemplo 4
+
+var soma1 = 10 + 10 + 20 + (30 * 4) / 2 + 10;
+console.log(soma1);
+
+//Exemplo 5
+
+var x = 5;
+console.log(x++);
+console.log(x);
+
+var y = 5;
+console.log(y--);
+console.log(y);
