f507702fd5b66d978fc9230238c0266f47f0c1c9
AndreLucasv10 <andrelucasv10@gmail.com>
2023-09-05 14:22:24 -0300

    pequeno estudo

---
+++ JavaScript para iniciantes/0201/Arrays e loops/script.js
@@ -0,0 +1,20 @@
+//Exemplo 1
+
+var videoGames = ["Switch", "PS4", "XBox"];
+
+videoGames[0]; // Switch
+videoGames[2]; // Xbox
+
+//Exemplo 2
+
+var videoGames = ["Switch", "PS4", "XBox"];
+
+videoGames.pop(); // Remove o último item e retorna ele
+videoGames.push("3DS"); // Adiciona ao final da array
+videoGames.length; // 3
+
+// Exemplo 3
+
+for (var numero = 0; numero <= 10; numero++) {
+  console.log(numero);
+}
