8cdd51c9afc113b4e7fa55f1c5bdad6f3d7fcae5
AndreLucasv10 <andrelucasv10@gmail.com>
2023-10-01 20:10:58 -0300

    Update script.js

--- Objetos/Number e Match/script.js
+++ Objetos/Number e Match/script.js
@@ -1,20 +1,20 @@
 // Retorne um número aleatório
 // entre 1050 e 2000
-
-
+const aleatório = Math.floor(Math.random() * (2000 - 1050 + 1)) + 1050;
+console.log(aleatório)
 // Retorne o maior número da lista abaixo
 const numeros = '4, 5, 20, 8, 9';
 
-
+numerosArray = numeros.split(',').map(numero => parseFloat(numero.trim()))
+console.log(Math.max.apply(null, numerosArray))
 // Crie uma função para limpar os preços
 // e retornar os números com centavos arredondados
 // depois retorne a soma total
 const listaPrecos = ['R$ 59,99', ' R$ 100,222',
     'R$ 230  ', 'r$  200']
 
-const natan = false
-if (natan === true) {
-    console.log('Natan é gay')
-} else {
-    console.log('natan não é gay')
+function limparPreco(preco) {
+    preco = +preco.toUpperCase().replace('R$', '').trim().replace(',', '.')
+    console.log(preco)
 }
+limparPreco(listaPrecos)
