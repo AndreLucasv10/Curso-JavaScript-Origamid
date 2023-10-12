9ef286b769d726d891263b91ffde0b389be1f2d6
André Lucas <88734289+AndreLucasv10@users.noreply.github.com>
2023-10-11 21:52:35 -0300

    Object

---
+++ Objetos/Object/Exercicio/script.js
@@ -0,0 +1,16 @@
+// Liste 5 objetos nativos
+//Object
+//String
+//Array
+//Number
+//Boolean
+// Liste 5 objetos do browser
+//width
+//Height
+//Pixel Depth
+//window.innerWidth;
+//window.innerHeight;
+// Liste 2 Métodos, Propriedades ou Objetos
+// presentes no Chrome que não existem no Firefox
+
+
---
+++ Objetos/Object/script.js
@@ -0,0 +1,76 @@
+const pessoa = new Object({
+  nome: 'André',
+  idade: 19
+})
+
+
+// Object.create, cria obejtos com o mesmo prototipo do objeto relacionado
+
+const carro = {
+  rodas: 4,
+
+  init(valor){
+  this.marca = valor
+  return this
+  },
+  acelerar() {
+    return this.marca + ' Acelerou'
+  },
+  buzinar(){
+    return this.marca + ' Buzinou'
+  }
+
+
+}
+
+const honda = Object.create(carro).init('Honda')
+console.log(honda.acelerar())
+
+// Object.Assign
+
+const funcaoAutomovel = {
+  acelerar() {
+    return 'acelerou';
+  },
+  buzinar() {
+    return 'buzinou';
+  },
+}
+
+const moto = {
+  rodas: 2,
+  capacete: true,
+}
+
+const carros = {
+  rodas: 4,
+  mala: true,
+}
+
+Object.assign(moto, funcaoAutomovel);
+Object.assign(carros, funcaoAutomovel);
+
+console.log(moto)
+console.log(carros)
+
+// Object.defineProperties
+const motos = {
+
+  capacete: true,
+}
+
+  Object.defineProperties(motos, {
+
+    rodas: {
+          get(){
+           return  this._rodas
+      },
+
+      set(valor){
+          this._rodas = valor * 4
+      },
+    }
+  })
+  console.log(motos)
+
+  motos.rodas = 4
