e41f71baf292fa786fa4d4724f052ffa80cbc924
André Lucas <88734289+AndreLucasv10@users.noreply.github.com>
2023-10-13 23:59:22 -0300

    aaa

--- blb/script.js
+++ blb/script.js
@@ -1,8 +1,13 @@
-function naoposso(){
+
+document.addEventListener("DOMContentLoaded", function() {
+    var meuBotao = document.getElementsByClassName("meuBotao");
+
+ function meuBotao() {
         var novaPosicaoX = Math.floor(Math.random() * (window.innerWidth - meuBotao.offsetWidth));
         var novaPosicaoY = Math.floor(Math.random() * (window.innerHeight - meuBotao.offsetHeight));
         
         meuBotao.style.left = novaPosicaoX + "px";
         meuBotao.style.top = novaPosicaoY + "px";
-}
-console.log(novaPosicaoX)
+    };
+});
+meuBotao()
