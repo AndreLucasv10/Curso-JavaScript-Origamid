e097a192511bced048b414242039ddb05de371e1
André Lucas <88734289+AndreLucasv10@users.noreply.github.com>
2023-10-17 15:54:46 -0300

    termino dos estudos

---
+++ Efeitos no Dom/Modulos/js/script.js
@@ -0,0 +1,8 @@
+import initScrollSuave from "./modules/scroll-suave.js";
+import initAnimacaoScroll from './modules/animacao-scroll.js'
+import initAccordion from './modules/accordion.js'
+import initTabNav from './modules/tabNav.js'
+initScrollSuave()
+initAnimacaoScroll()
+initAccordion()
+initTabNav()
