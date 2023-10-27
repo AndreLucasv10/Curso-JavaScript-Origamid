8b59649a0df38987e978466b393f42264b8c460d
André Lucas <88734289+AndreLucasv10@users.noreply.github.com>
2023-10-27 17:08:21 -0300

    estudos

---
+++ Efeitos no Dom/Data-object/js/modules/data-object.js
@@ -0,0 +1,16 @@
+export default function initFuncionamento(){
+
+}
+const funcionamento = document.querySelector('[data-semana]')
+const diasSemana = funcionamento.dataset.semana.split(',').map(Number)
+const horarioSemana = funcionamento.dataset.horario.split(',').map(Number)
+console.log(diasSemana)
+console.log(horarioSemana)
+
+const dataAgora = new Date()
+const diaAgora = dataAgora.getDay()
+const horaAgora = dataAgora.getHours()
+const teste = [1,2,3,4,5].indexOf(5)
+
+const semanaAberto = diasSemana.indexOf(diaAgora) !== -1
+console.log(semanaAberto)
